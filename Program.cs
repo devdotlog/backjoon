b61d75af497dce972ae761cfa95d8a0ba1ef1b06
devdotlog <ssh0702@gmail.com>
2018-06-12 00:52:55 +0900

    https://www.acmicpc.net/problem/2839

--- Program.cs
+++ Program.cs
@@ -4,9 +4,30 @@ namespace backjoon
 {
     class Program
     {
+        // https://www.acmicpc.net/problem/2839
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine().Split()[0];
+            int sugar = int.Parse(input);
+            int packages = 0;
+
+            while (sugar % 5 > 0)
+            {
+                if (sugar > 5 || sugar == 3)
+                {
+                    sugar -= 3;
+                    packages++;
+                }
+                else if (sugar < 5)
+                {
+                    Console.WriteLine("-1");
+                    return;
+                }
+            }
+
+            packages = sugar / 5 + packages;
+
+            Console.WriteLine(packages);
         }
     }
 }

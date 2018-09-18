a90eedba779f90cbd234c1e85f37d0264db5ee26
shseo@digicaps.com <shseo@digicaps.com>
2018-09-18 13:21:50 +0900

    https://www.acmicpc.net/submit/2439

---
+++ src/2439.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int
+	fmt.Scanf("%d", &num)
+
+	for i := 1; i <= num; i++ {
+		for j := i; j < num; j++ {
+			fmt.Print(" ")
+		}
+		for j := num - i; j < num; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
